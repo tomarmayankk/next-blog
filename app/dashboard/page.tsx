69c0fff2179efc7e431cd72c057d6d25e837e5e7
tomarmayankk <mayanktomarpc@gmail.com>
2025-03-19 21:53:03 +0530

    edit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,7 +19,7 @@ async function getData(userId: string) {
 async function DashboardRoute() {
   const {getUser} = getKindeServerSession();
   const user = await getUser();
-  const data = await getData(user.id);
+  const data = await getData(user?.id);
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
